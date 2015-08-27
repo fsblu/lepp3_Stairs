415e067c3d1626a419b080a6e72a01b41065b16b
Irem Uygur <iremuygur1@gmail.com>
2015-08-27 15:57:14 +0200

    stairs

---
+++ lepp2/StairSegmenter.hpp
@@ -0,0 +1,402 @@
+#ifndef LEPP2_STAIR_SEGMENTER_H__
+#define LEPP2_STAIR_SEGMENTER_H__
+
+#include "lepp2/BaseSegmenter.hpp"
+
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+
+namespace lepp {
+
+/**
+ * TODO put comments
+ */
+template<class PointT>
+class StairSegmenter: public BaseSegmenter<PointT> {
+public:
+	StairSegmenter();
+
+	virtual std::vector<typename pcl::PointCloud<PointT>::ConstPtr> segment(
+			const typename pcl::PointCloud<PointT>::ConstPtr& cloud);
+private:
+	// Helper typedefs to make the implementation code cleaner
+	typedef pcl::PointCloud<PointT> PointCloudT;
+	typedef typename PointCloudT::Ptr PointCloudPtr;
+	typedef typename PointCloudT::ConstPtr CloudConstPtr;
+
+	// Private helper member functions
+	/**
+	 * Performs some initial preprocessing and filtering appropriate for the
+	 * segmentation algorithm.
+	 * Takes the original cloud as a parameter and returns a pointer to a newly
+	 * created (and allocated) cloud containing the result of the filtering.
+	 */
+	PointCloudPtr preprocessCloud(CloudConstPtr const& cloud);
+	/**
+	 * Removes all planes from the given point cloud.
+	 */
+	void findStairs(PointCloudPtr const& cloud_filtered);
+	/**
+	 * Extracts the Euclidean clusters from the given point cloud.
+	 * Returns a vector where each element represents the pcl::PointIndices
+	 * instance representing the corresponding cluster.
+	 */
+	std::vector<pcl::PointIndices> getStairClusters();
+	/**
+	 * Convert the clusters represented by the given indices to point clouds,
+	 * by copying the corresponding points from the cloud to the corresponding
+	 * new point cloud.
+	 */
+	std::vector<CloudConstPtr> clustersToPointClouds(
+			std::vector<pcl::PointIndices> const& cluster_indices);
+
+	/* Classification function between segmentation and clustering steps.
+	 * Classify any segmented planar plane into one of the segmented surface group,
+	 * regarding its plane normal.
+	 * This function is necessary to be able to separate ramps or any inclined
+	 * surface from the floor at the clustering step.
+	 * */
+
+	void handleRecongnizedSurface(PointCloudPtr const& cloud_planar_surface,
+			const pcl::ModelCoefficients & coeffs);
+
+	/*Returns the angle between two plane normals.
+	 * Calculation is made based on plane coefficients
+	 */
+	double getAngle(const pcl::ModelCoefficients &coeffs, const int &index);
+
+	/*
+	 *This function is called if a plane whose plane normal differs more
+	 *than 3 degrees with the saved segmented plane groups, is segmented.The
+	 *plane and its coefficients are saved under a new plane group.
+	 * */
+
+	bool addCoefficient(const int surface_number,
+			const PointCloudPtr cloud_planar_surface,
+			const pcl::ModelCoefficients coeffs);
+
+    /*This function is called to add the segmented plane to a corresponding
+     *surface group. Surface planes normals devising 3 degrees are considered same
+     *type of surface and saved together.
+     * */
+	bool addPlane(int const index,
+			const PointCloudPtr cloud_planar_surface);
+	/**
+		 * Instance used to extract the planes from the input cloud.
+		 */
+	pcl::SACSegmentation<PointT> segmentation_;
+	/**
+	 * Instance used to extract the actual clusters from the input cloud.
+	 */
+	pcl::EuclideanClusterExtraction<PointT> clusterizer_;
+	/**
+	 * The KdTree will hold the representation of the point cloud which is passed
+	 * to the clusterizer.
+	 */
+	boost::shared_ptr<pcl::search::KdTree<PointT> > kd_tree_;
+
+	/**
+	 * The cloud that holds all planar surfaces (Stairs).
+	 */
+	PointCloudPtr cloud_stairs_;
+	/**
+	 * The percentage of the original cloud that should be kept for the
+	 * clusterization, at the least.
+	 * We stop removing planes from the original cloud once there are either no
+	 * more planes to be removed or when the number of points remaining in the
+	 * cloud dips below this percentage of the original cloud.
+	 */
+
+	/**
+	 * TEMP: vector containing clouds for each stair
+	 */
+	std::vector<CloudConstPtr> vec_cloud_stairs_;
+
+	std::vector<PointCloudPtr> vec_surface;
+
+	/* List of plane coefficients [normal_x normal_y normal_z hessian_component_d]
+	 *
+	 */
+	std::vector<pcl::ModelCoefficients> m_coefficients;
+
+	/*Segmentation ratio*/
+	double const min_filter_percentage_;
+};
+
+template<class PointT>
+StairSegmenter<PointT>::StairSegmenter() :
+		min_filter_percentage_(0.1), kd_tree_(
+				new pcl::search::KdTree<PointT>()), cloud_stairs_(
+				new PointCloudT()) {
+	// Parameter initialization of the plane segmentation
+	segmentation_.setOptimizeCoefficients(true);
+	segmentation_.setModelType(pcl::SACMODEL_PLANE);
+	segmentation_.setMethodType(pcl::SAC_RANSAC);
+	segmentation_.setMaxIterations(100); // value recognized by Irem
+	segmentation_.setDistanceThreshold(0.03);
+
+	// Parameter initialization of the clusterizer
+	clusterizer_.setClusterTolerance(0.02);
+	clusterizer_.setMinClusterSize(1800);
+	clusterizer_.setMaxClusterSize(30000);
+}
+
+template<class PointT>
+typename pcl::PointCloud<PointT>::Ptr StairSegmenter<PointT>::preprocessCloud(
+		CloudConstPtr const& cloud) {
+	// Remove NaN points from the input cloud.
+	// The pcl API forces us to pass in a reference to the vector, even if we have
+	// no use of it later on ourselves.
+	PointCloudPtr cloud_filtered(new PointCloudT());
+	std::vector<int> index;
+	pcl::removeNaNFromPointCloud<PointT>(*cloud, *cloud_filtered, index);
+
+	return cloud_filtered;
+}
+
+template<class PointT>
+void StairSegmenter<PointT>::findStairs(PointCloudPtr const& cloud_filtered) {
+
+	vec_cloud_stairs_.clear();
+	m_coefficients.clear();
+
+	// Instance that will be used to perform the elimination of unwanted points
+	// from the point cloud.
+	pcl::ExtractIndices<PointT> extract;
+	// Will hold the indices of the next extracted plane within the loop
+	pcl::PointIndices::Ptr current_plane_indices(new pcl::PointIndices);
+	// Another instance of when the pcl API requires a parameter that we have no
+	// further use for.
+	pcl::ModelCoefficients coefficients;
+	// Remove planes until we reach x % of the original number of points
+	size_t const original_cloud_size = cloud_filtered->size();
+	size_t const point_threshold = min_filter_percentage_ * original_cloud_size;
+	cloud_stairs_->clear();
+	while (cloud_filtered->size() > point_threshold) {
+		// Try to obtain the next plane...
+		segmentation_.setInputCloud(cloud_filtered);
+		segmentation_.segment(*current_plane_indices, coefficients);
+
+		// We didn't get any plane in this run. Therefore, there are no more planes
+		// to be removed from the cloud.
+		if (current_plane_indices->indices.size() == 0) {
+			std::cout << "cannot find more planes > BREAK" << std::endl;
+			break;
+		}
+
+		// Cloud that holds a plane in each iteration, to be added to the total cloud.
+		PointCloudPtr cloud_planar_surface(new PointCloudT());
+
+		// Add the planar inliers to the cloud holding the stairs
+		extract.setInputCloud(cloud_filtered);
+		extract.setIndices(current_plane_indices);
+		extract.setNegative(false);
+		extract.filter(*cloud_planar_surface);
+
+		// ... and remove those inliers from the input cloud
+		extract.setNegative(true);
+		extract.filter(*cloud_filtered);
+
+		*cloud_stairs_ += *cloud_planar_surface;
+
+		//Classify the Cloud
+
+		//vec_cloud_stairs_.push_back(cloud_planar_surface);
+	}
+
+	std::cout << "#found Stairs: " << vec_cloud_stairs_.size() << std::endl;
+}
+
+template<class PointT>
+std::vector<pcl::PointIndices> StairSegmenter<PointT>::getStairClusters() {
+	// Extract the clusters from such a filtered cloud.
+	kd_tree_->setInputCloud(cloud_stairs_);
+	clusterizer_.setSearchMethod(kd_tree_);
+	clusterizer_.setInputCloud(cloud_stairs_);
+	std::vector<pcl::PointIndices> cluster_indices;
+	clusterizer_.extract(cluster_indices);
+
+	return cluster_indices;
+}
+
+template<class PointT> double StairSegmenter<PointT>::getAngle(
+		const pcl::ModelCoefficients &coeffs, const int &index) {
+
+	//Scalar Product
+	float scalar_product = (m_coefficients.at(index).values[0]
+			* coeffs.values[0])
+			+ (m_coefficients.at(index).values[1] * coeffs.values[1])
+			+ (m_coefficients.at(index).values[2] * coeffs.values[2]);
+	double angle = acos(scalar_product) * 180.0 / M_PI;
+	cout << "The angle from scalar product: " << angle << std::endl;
+
+	return angle;
+}
+template<class PointT>
+void StairSegmenter<PointT>::handleRecongnizedSurface(
+		PointCloudPtr const& cloud_planar_surface,
+		const pcl::ModelCoefficients & coeffs) {
+
+	bool coeff_exists = false;
+
+	int surface_num = m_coefficients.size();
+
+	for (int i = 0; i < surface_num; i++) {
+		cout << "Angle: " << getAngle(coeffs, i) << std::endl;
+
+		if (getAngle(coeffs, i) < 3) {
+
+			cout << "Expected addition to surface: " << i << std::endl;
+			cout << "Size of the plane: " << cloud_planar_surface->points.size()
+					<< std::endl;
+			coeff_exists = addPlane(i, cloud_planar_surface);
+			break;
+		}
+	}
+
+	if (coeff_exists == false) {
+
+		coeff_exists = addCoefficient(surface_num, cloud_planar_surface,
+				coeffs);
+	}
+
+	if (coeff_exists == false)
+		return false;
+	return true;
+}
+template<Class PointT>
+bool StairSegmenter<PointT>::addCoefficient(const int surface_number,
+		const PointCloudPtr cloud_planar_surface,
+		const pcl::ModelCoefficients coeffs) {
+
+	int size = cloud_planar_surface->points.size();
+	//cout << "Size: " << size << std::endl;
+	bool isPlaneAdded = false;
+	switch (surface_number) {
+	case 0:
+
+		*surface_0 += (*cloud_planar_surface);
+		//cout << "Right before" << std::endl;
+		addClusterParameters(true, 0, size);
+		//cout << "The first surface is added" << std::endl;
+		isPlaneAdded = true;
+		break;
+	case 1:
+
+		*surface_1 += (*cloud_planar_surface);
+		//cout << "Surface_1++" << std::endl;
+		addClusterParameters(true, 1, size);
+		isPlaneAdded = true;
+		break;
+	case 2:
+
+		*surface_2 += (*cloud_planar_surface);
+		addClusterParameters(true, 2, size);
+		//cout << "Surface_2++" << std::endl;
+		isPlaneAdded = true;
+		break;
+	case 3:
+
+		*surface_3 += (*cloud_planar_surface);
+		addClusterParameters(true, 3, size);
+		//cout << "Surface_3++" << std::endl;
+		isPlaneAdded = true;
+		break;
+	default:
+//		cout << "ERROR Could not add new coefficients and surface" << std::endl;
+		isPlaneAdded = false;
+		break;
+	}
+
+	if (isPlaneAdded) {
+		m_coefficients->push_back(coeffs);
+//		cout << "New Coefficient Added! Surface_number : " << surface_number
+//		<< std::endl;
+		return false;
+	}
+	return false;
+
+}
+
+template <class PointT>
+bool StairSegmenter<PointT>::addPlane(int const index,
+		const PointCloudPtr cloud_planar_surface) {
+	int size = cloud_planar_surface->points.size();
+	switch (index) {
+
+	case 0:
+		*surface_0 += (*cloud_planar_surface);
+
+		addClusterParameters(false, 0, size);
+		//cout << "Added to the " "Floor" "surface_0" << std::endl;
+		return true;
+	case 1:
+		*surface_1 += (*cloud_planar_surface);
+		addClusterParameters(false, 1, size);
+		//cout << "Added to surface_1" << std::endl;
+
+		return true;
+	case 2:
+		*surface_2 += (*cloud_planar_surface);
+		addClusterParameters(false, 2, size);
+		//cout << "Added to surface_2" << std::endl;
+
+		return true;
+		break;
+	case 3:
+		*surface_3 += (*cloud_planar_surface);
+		addClusterParameters(false, 3, size);
+		//cout << "Added to surface_3" << std::endl;
+
+		return true;
+	default:
+		cout << "Could not add to existing point clouds" << std::endl;
+		return true;
+	} //Switch
+
+	cout << "ERROR IN PLANE ADDITION" << std::endl;
+	return false;
+}
+template<class PointT>
+std::vector<typename pcl::PointCloud<PointT>::ConstPtr> StairSegmenter<PointT>::clustersToPointClouds(
+		std::vector<pcl::PointIndices> const& cluster_indices) {
+	// Now copy the points belonging to each cluster to a separate PointCloud
+	// and finally return a vector of these point clouds.
+	//std::vector<CloudConstPtr> ret;
+	size_t const cluster_count = cluster_indices.size();
+	for (size_t i = 0; i < cluster_count; ++i) {
+
+		typename PointCloudT::Ptr current(new PointCloudT());
+		std::vector<int> const& curr_indices = cluster_indices[i].indices;
+		size_t const curr_indices_sz = curr_indices.size();
+		for (size_t j = 0; j < curr_indices_sz; j++) {
+			// add the point to the corresponding point cloud
+			current->push_back(cloud_stairs_->at(curr_indices[j]));
+		}
+
+		vec_cloud_stairs_.push_back(current);
+	}
+
+	std::cout << "Stairs number:" << vec_cloud_stairs_.size() << std::endl;
+	return vec_cloud_stairs_;
+}
+
+template<class PointT>
+std::vector<typename pcl::PointCloud<PointT>::ConstPtr> StairSegmenter<PointT>::segment(
+		const typename pcl::PointCloud<PointT>::ConstPtr& cloud) {
+	PointCloudPtr cloud_filtered = preprocessCloud(cloud);
+	// extract those planes that are considered as stairs and put them in cloud_stairs_
+	findStairs(cloud_filtered);
+	std::vector<pcl::PointIndices> cluster_indices = getStairClusters();
+	return clustersToPointClouds(cluster_indices);
+
+	return vec_cloud_stairs_;
+//  std::vector<pcl::PointIndices> stair_cluster_indices = getStairClusters(cloud_stairs_);
+//  return clustersToPointClouds(cloud_stairs_, stair_cluster_indices);
+}
+
+} // namespace lepp
+
+#endif
---
+++ lepp2/detector/main.cc
@@ -0,0 +1,138 @@
+/**
+ * A program performing detection of obstacles in the given file feed
+ * and visualizing their approximations.
+ */
+#include <iostream>
+
+#include <pcl/io/openni2_grabber.h>
+#include <pcl/io/pcd_grabber.h>
+
+#include "lepp2/BaseObstacleDetector.hpp"
+#include "lepp2/StairDetector.hpp"
+#include "lepp2/GrabberVideoSource.hpp"
+#include "lepp2/BaseVideoSource.hpp"
+#include "lepp2/VideoObserver.hpp"
+#include "lepp2/FilteredVideoSource.hpp"
+#include "lepp2/SmoothObstacleAggregator.hpp"
+
+#include "lepp2/visualization/EchoObserver.hpp"
+#include "lepp2/visualization/ObstacleVisualizer.hpp"
+#include "lepp2/visualization/StairVisualizer.hpp"
+
+#include "lepp2/filter/TruncateFilter.hpp"
+#include "lepp2/filter/SensorCalibrationFilter.hpp"
+
+#include "lepp2/models/ObjectModel.h"
+
+using namespace lepp;
+
+namespace {
+  typedef SimplePoint PointT;
+}
+
+/**
+ * Prints out the expected CLI usage of the program.
+ */
+void PrintUsage() {
+  std::cout << "usage: detector [--pcd file | --oni file | --stream]"
+      << std::endl;
+  std::cout << "--pcd    : " << "read the input from a .pcd file" << std::endl;
+  std::cout << "--oni    : " << "read the input from an .oni file" << std::endl;
+  std::cout << "--stream : " << "read the input from a live stream based on a"
+      << " sensor attached to the computer" << std::endl;
+}
+
+/**
+ * Builds a `FilteredVideoSource` instance that wraps the given raw source.
+ */
+template<class PointT>
+boost::shared_ptr<FilteredVideoSource<PointT> >
+buildFilteredSource(boost::shared_ptr<VideoSource<PointT> > raw) {
+  // Wrap the given raw source.
+  boost::shared_ptr<FilteredVideoSource<PointT> > source(
+      new SimpleFilteredVideoSource<PointT>(raw));
+  // Now set the point filters that should be used.
+  {
+    double const a = 1.0117;
+    double const b = -0.0100851;
+    boost::shared_ptr<PointFilter<PointT> > filter(
+        new SensorCalibrationFilter<PointT>(a, b));
+    source->addFilter(filter);
+  }
+  {
+    boost::shared_ptr<PointFilter<PointT> > filter(
+        new TruncateFilter<PointT>(2)); // STEP SIZE --> 1 CM
+    source->addFilter(filter);
+  }
+
+  return source;
+}
+
+/**
+ * Parses the command line arguments received by the program and chooses
+ * the appropriate video source based on those.
+ */
+boost::shared_ptr<VideoSource<PointT> > GetVideoSource(int argc, char* argv[]) {
+  if (argc < 2) {
+    return boost::shared_ptr<VideoSource<PointT> >();
+  }
+
+  std::string const option = argv[1];
+  if (option == "--stream") {
+    return boost::shared_ptr<VideoSource<PointT> >(
+        new LiveStreamSource<PointT>());
+  } else if (option == "--pcd" && argc >= 3) {
+    std::string const file_path = argv[2];
+    boost::shared_ptr<pcl::Grabber> interface(new pcl::PCDGrabber<PointT>(
+      file_path,
+      20.,
+      true));
+    return boost::shared_ptr<VideoSource<PointT> >(
+        new GeneralGrabberVideoSource<PointT>(interface));
+  } else if (option == "--oni" && argc >= 3) {
+    std::string const file_path = argv[2];
+    boost::shared_ptr<pcl::Grabber> interface(new pcl::io::OpenNI2Grabber(
+      file_path,
+      pcl::io::OpenNI2Grabber::OpenNI_Default_Mode,
+      pcl::io::OpenNI2Grabber::OpenNI_Default_Mode));
+    return boost::shared_ptr<VideoSource<PointT> >(
+        new GeneralGrabberVideoSource<PointT>(interface));
+  }
+
+  // Unknown option: return a "null" pointer.
+  return boost::shared_ptr<VideoSource<PointT> >();
+}
+
+int main(int argc, char* argv[]) {
+  // Obtain a video source based on the command line arguments received
+  boost::shared_ptr<VideoSource<PointT> > raw_source(GetVideoSource(argc, argv));
+  if (!raw_source) {
+    PrintUsage();
+    return 1;
+  }
+  // Wrap the raw source in a filter
+  boost::shared_ptr<FilteredVideoSource<PointT> > source(
+      buildFilteredSource(raw_source));
+  // Prepare the detector
+  boost::shared_ptr<StairDetector<PointT> > stairDetector(
+      new StairDetector<PointT>());
+  source->attachObserver(stairDetector);
+
+  // Prepare the result visualizer...
+  boost::shared_ptr<StairVisualizer<PointT> > visualizer(
+        new StairVisualizer<PointT>());
+  // Attaching the visualizer to the source: allow it to display the original
+  // point cloud.
+  source->attachObserver(visualizer);
+  stairDetector->attachStairAggregator(visualizer);
+
+  // Starts capturing new frames and forwarding them to attached observers.
+  source->open();
+
+  std::cout << "Waiting forever..." << std::endl;
+  std::cout << "(^C to exit)" << std::endl;
+  while (true)
+    boost::this_thread::sleep(boost::posix_time::milliseconds(8000));
+
+  return 0;
+}
